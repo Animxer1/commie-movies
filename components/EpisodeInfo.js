0796e5d5ff28f3508d71d7966bcdaaefabcea0b5
TheHolyTachanka <TheOneSlav@proton.me>
2023-05-07 20:16:17 +0000

    made EpisodeInfo and MovieCards better

--- components/EpisodeInfo.js
+++ components/EpisodeInfo.js
@@ -1,39 +1,44 @@
+// Import necessary hooks and components
 import { useState, useEffect } from "react";
 import Link from "next/link";
 
+// EpisodeInfo component that displays episode details and navigation links
 const EpisodeInfo = ({ EpisodeDetail, seasonid, epid, showid }) => {
-  const embedUrl = process.env.EMBED_URL;
-  const [prevEp, setPrevEp] = useState(null);
-  const [nextEp, setNextEp] = useState(null);
+  // Destructure episode details
+  const { name, air_date, overview, vote_average, vote_count } = EpisodeDetail;
 
+  // Initialize state for episode navigation
+  const [episodeNav, setEpisodeNav] = useState({ prevEp: null, nextEp: null });
+
+  // Update episode navigation links when epid changes
   useEffect(() => {
-    setPrevEp(epid > 1 ? epid - 1 : null);
-    setNextEp(parseInt(epid) + 1);
+    setEpisodeNav({
+      prevEp: epid > 1 ? epid - 1 : null,
+      nextEp: parseInt(epid) + 1,
+    });
   }, [epid]);
 
+  // Render the component
   return (
     <div className="episode-details container mx-auto px-4 py-11 md:mx-24">
       <div className="flex flex-col md:flex-row episode-container justify-center items-center">
         <div className="w-full md:w-1/2 md:mr-10">
           <h2 className="text-4xl font-semibold text-white mb-4">
-            {seasonid} x {epid} : {EpisodeDetail.name}
+            {seasonid} x {epid} : {name}
           </h2>
-          <div className="text-white text-sm mb-4">
-            {EpisodeDetail.air_date}
-          </div>
-          <p className="text-white text-lg leading-7 mb-8">
-            {EpisodeDetail.overview}
-          </p>
+          <div className="text-white text-sm mb-4">{air_date}</div>
+          <p className="text-white text-lg leading-7 mb-8">{overview}</p>
           <div className="flex items-center text-white text-sm mb-4">
-            <span>{EpisodeDetail.vote_average}</span>
+            <span>{vote_average}</span>
             <span className="mx-2">|</span>
-            <span>{EpisodeDetail.vote_count} votes</span>
+            <span>{vote_count} votes</span>
           </div>
+          {/* Render navigation links based on episodeNav state */}
           <div className="flex justify-center">
-            {prevEp && (
+            {episodeNav.prevEp && (
               <Link
                 legacyBehavior
-                href={`/tv/${showid}/season/${seasonid}/${prevEp}`}
+                href={`/tv/${showid}/season/${seasonid}/${episodeNav.prevEp}`}
               >
                 <a className="bg-gray-900 text-white px-6 py-3 rounded-md mr-4">
                   Previous Episode
@@ -41,10 +46,10 @@ const EpisodeInfo = ({ EpisodeDetail, seasonid, epid, showid }) => {
               </Link>
             )}
 
-            {nextEp && (
+            {episodeNav.nextEp && (
               <Link
                 legacyBehavior
-                href={`/tv/${showid}/season/${seasonid}/${nextEp}`}
+                href={`/tv/${showid}/season/${seasonid}/${episodeNav.nextEp}`}
               >
                 <a className="bg-gray-900 text-white px-6 py-3 rounded-md">
                   Next Episode
@@ -53,6 +58,7 @@ const EpisodeInfo = ({ EpisodeDetail, seasonid, epid, showid }) => {
             )}
           </div>
         </div>
+        {/* Render video player */}
         <div className="w-full md:w-1/2">
           <div className="aspect-w-16 aspect-h-9">
             <iframe
@@ -68,4 +74,5 @@ const EpisodeInfo = ({ EpisodeDetail, seasonid, epid, showid }) => {
   );
 };
 
+// Export the component
 export default EpisodeInfo;
--- components/MovieCards.js
+++ components/MovieCards.js
@@ -1,24 +1,23 @@
 import Link from "next/link";
 
 const MovieCards = ({ MovieCard }) => {
-  let poster_path = `https://image.tmdb.org/t/p/w342${
+  const poster_path = `https://image.tmdb.org/t/p/w342${
     MovieCard.poster_path ?? "/wjVuAGb.png"
   }`;
+
   return (
     <div className="my-2 px-2 w-full overflow-hidden sm:my-2 sm:px-2 sm:w-1/2 md:my-2 md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/5 xl:my-2 xl:px-2 xl:w-1/5">
       <div className="rounded-md overflow-hidden">
-        <Link
-          legacyBehavior
-          key={MovieCard.id}
-          href="/movie/[id]"
-          as={`/movie/${MovieCard.id}`}
-        >
+        <Link legacyBehavior key={MovieCard.id} href="/movie/[id]" as={`/movie/${MovieCard.id}`}>
           <a title={MovieCard.title}>
             <img
               className="w-full h-auto hover:opacity-70 rounded-md"
               title={MovieCard.title}
               src={poster_path}
               alt={MovieCard?.title}
+              layout="responsive"
+              width={342}
+              height={513}
             />
           </a>
         </Link>
