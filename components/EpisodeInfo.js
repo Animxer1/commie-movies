d4ce6edbe4b7df2972e6f498627e778c5271593a
TheHolyTachanka <TheOneSlav@proton.me>
2023-05-08 09:38:36 +0000

    improved EpisodeInfo

--- components/EpisodeInfo.js
+++ components/EpisodeInfo.js
@@ -1,14 +1,10 @@
-// Import necessary hooks and components
 import { useState, useEffect } from "react";
 import Link from "next/link";
 
-// EpisodeInfo component that displays episode details and navigation links
 const EpisodeInfo = ({ EpisodeDetail, seasonid, epid, showid }) => {
-  // Destructure episode details
   const { name, air_date, overview, vote_average, vote_count } = EpisodeDetail;
-  // Initialize state for episode navigation
   const [episodeNav, setEpisodeNav] = useState({ prevEp: null, nextEp: null });
-  // Update episode navigation links when epid changes
+
   useEffect(() => {
     setEpisodeNav({
       prevEp: epid > 1 ? epid - 1 : null,
@@ -16,7 +12,12 @@ const EpisodeInfo = ({ EpisodeDetail, seasonid, epid, showid }) => {
     });
   }, [epid]);
 
-  // Render the component
+  const renderNavLink = (linkText, linkHref) => (
+    <Link legacyBehavior href={linkHref}>
+      <a className="bg-gray-900 text-white px-6 py-3 rounded-md mr-4">{linkText}</a>
+    </Link>
+  );
+
   return (
     <div className="episode-details container mx-auto px-4 py-11 md:mx-24">
       <div className="flex flex-col md:flex-row episode-container justify-center items-center">
@@ -31,32 +32,14 @@ const EpisodeInfo = ({ EpisodeDetail, seasonid, epid, showid }) => {
             <span className="mx-2">|</span>
             <span>{vote_count} votes</span>
           </div>
-          {/* Render navigation links based on episodeNav state */}
           <div className="flex justify-center">
-            {episodeNav.prevEp && (
-              <Link
-                legacyBehavior
-                href={`/tv/${showid}/season/${seasonid}/${episodeNav.prevEp}`}
-              >
-                <a className="bg-gray-900 text-white px-6 py-3 rounded-md mr-4">
-                  Previous Episode
-                </a>
-              </Link>
-            )}
+            {episodeNav.prevEp &&
+              renderNavLink("Previous Episode", `/tv/${showid}/season/${seasonid}/${episodeNav.prevEp}`)}
 
-            {episodeNav.nextEp && (
-              <Link
-                legacyBehavior
-                href={`/tv/${showid}/season/${seasonid}/${episodeNav.nextEp}`}
-              >
-                <a className="bg-gray-900 text-white px-6 py-3 rounded-md">
-                  Next Episode
-                </a>
-              </Link>
-            )}
+            {episodeNav.nextEp &&
+              renderNavLink("Next Episode", `/tv/${showid}/season/${seasonid}/${episodeNav.nextEp}`)}
           </div>
         </div>
-        {/* Render video player */}
         <div className="w-full md:w-1/2">
           <div className="aspect-w-16 aspect-h-9">
             <iframe
@@ -72,5 +55,4 @@ const EpisodeInfo = ({ EpisodeDetail, seasonid, epid, showid }) => {
   );
 };
 
-// Export the component
 export default EpisodeInfo;
