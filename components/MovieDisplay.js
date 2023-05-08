913f4fd801bc38baef8cfc9df5a813ead27e0122
TheHolyTachanka <TheOneSlav@proton.me>
2023-05-08 09:56:34 +0000

    Added comments to MovieDisplay

--- components/MovieDisplay.js
+++ components/MovieDisplay.js
@@ -2,9 +2,12 @@ import Link from "next/link";
 import MovieCard from "./MovieCards";
 
 const MovieDisplay = ({ movie, pageid }) => {
+  // Create an array of page numbers
   const pagenum = Array.from({ length: 10 }, (_, i) => i + 1);
+  // Ensure pageid is a number and defaults to 1 if not
   pageid = isNaN(pageid) ? 1 : +pageid;
 
+  // Render a single pagination item
   const renderPaginationItem = (element) => (
     <Link legacyBehavior key={element} href={`/movie/popular/page/${element}`} passHref>
       <a
@@ -19,6 +22,7 @@ const MovieDisplay = ({ movie, pageid }) => {
 
   return (
     <div className="popular-movies md:mx-24">
+      {/* Render movie cards */}
       <div className="flex flex-wrap overflow-hidden sm:-mx-2 pl-2 md:-mx-2 lg:-mx-2 xl:-mx-2">
         {movie?.map((movieCard) => (
           <MovieCard
@@ -28,6 +32,7 @@ const MovieDisplay = ({ movie, pageid }) => {
           />
         ))}
       </div>
+      {/* Render pagination */}
       <div className="flex justify-center mt-8 space-x-4 mb-10">
         <Link legacyBehavior href={`/movie/popular/page/${pageid - 1}`} passHref>
           <a className="bg-gray-800 text-white rounded-full px-4 py-2 hover:bg-gray-700">
