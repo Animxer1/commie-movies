93d429838185adaf7f3c4a228699cae3e60bd4b9
TheHolyTachanka <TheOneSlav@proton.me>
2023-05-08 09:50:09 +0000

    Added comments to MovieCards

--- components/MovieCards.js
+++ components/MovieCards.js
@@ -1,25 +1,27 @@
-// Import the Link component from the Next.js library
+// Import the Link component from Next.js
 import Link from "next/link";
 
-// Define the MovieCards functional component that takes a prop named MovieCard
+// MovieCards component receives a MovieCard object as a prop
 const MovieCards = ({ MovieCard }) => {
-  // Construct the poster_path by combining the base URL and the poster_path from the MovieCard object
-  // If the poster_path is not available, use a default image
+  // Construct the poster_path using a ternary operator to handle missing poster_path values
   const poster_path = `https://image.tmdb.org/t/p/w342${
     MovieCard.poster_path ?? "/wjVuAGb.png"
   }`;
-
   // Return the JSX for the MovieCards component
   return (
-    // Define the container div with appropriate responsive classes for different screen sizes
     <div className="my-2 px-2 w-full overflow-hidden sm:my-2 sm:px-2 sm:w-1/2 md:my-2 md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/5 xl:my-2 xl:px-2 xl:w-1/5">
-      // Define a rounded div for the movie poster
       <div className="rounded-md overflow-hidden">
-        // Use the Next.js Link component to navigate to the movie's detail page
-        <Link legacyBehavior href={`/movie/\${MovieCard.id}`}>
-          // Add an anchor tag with the movie title as the title attribute
+        {/* Use the Link component for client-side navigation */}
+        {/* TODO: remove legacyBehavior */}
+        <Link
+          legacyBehavior
+          key={MovieCard.id}
+          href="/movie/[id]"
+          as={`/movie/${MovieCard.id}`}
+        >
+          {/* Create an anchor tag with the movie's title as the title attribute */}
           <a title={MovieCard.title}>
-            // Add the movie poster image with hover effect and responsive sizing
+            {/* Display the movie poster image */}
             <img
               className="w-full h-auto hover:opacity-70 rounded-md"
               title={MovieCard.title}
