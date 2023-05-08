459e4b077f794bd070f382a593a501a548185831
TheHolyTachanka <TheOneSlav@proton.me>
2023-05-08 09:53:18 +0000

    Improved MovieDisplay

--- components/MovieDisplay.js
+++ components/MovieDisplay.js
@@ -4,6 +4,19 @@ import MovieCard from "./MovieCards";
 const MovieDisplay = ({ movie, pageid }) => {
   const pagenum = Array.from({ length: 10 }, (_, i) => i + 1);
   pageid = isNaN(pageid) ? 1 : +pageid;
+
+  const renderPaginationItem = (element) => (
+    <Link legacyBehavior key={element} href={`/movie/popular/page/${element}`} passHref>
+      <a
+        className={`bg-gray-800 text-white rounded-full px-4 py-2 hover:bg-gray-700 ${
+          pageid === element ? "bg-gray-700" : ""
+        }`}
+      >
+        {element}
+      </a>
+    </Link>
+  );
+
   return (
     <div className="popular-movies md:mx-24">
       <div className="flex flex-wrap overflow-hidden sm:-mx-2 pl-2 md:-mx-2 lg:-mx-2 xl:-mx-2">
@@ -16,36 +29,13 @@ const MovieDisplay = ({ movie, pageid }) => {
         ))}
       </div>
       <div className="flex justify-center mt-8 space-x-4 mb-10">
-        <Link
-          legacyBehavior
-          href={`/movie/popular/page/${pageid - 1}`}
-          passHref
-        >
+        <Link legacyBehavior href={`/movie/popular/page/${pageid - 1}`} passHref>
           <a className="bg-gray-800 text-white rounded-full px-4 py-2 hover:bg-gray-700">
             &laquo; Prev
           </a>
         </Link>
-        {pagenum.map((element) => (
-          <Link
-            legacyBehavior
-            key={element}
-            href={`/movie/popular/page/${element}`}
-            passHref
-          >
-            <a
-              className={`bg-gray-800 text-white rounded-full px-4 py-2 hover:bg-gray-700 ${
-                pageid === element ? "bg-gray-700" : ""
-              }`}
-            >
-              {element}
-            </a>
-          </Link>
-        ))}
-        <Link
-          legacyBehavior
-          href={`/movie/popular/page/${pageid + 1}`}
-          passHref
-        >
+        {pagenum.map(renderPaginationItem)}
+        <Link legacyBehavior href={`/movie/popular/page/${pageid + 1}`} passHref>
           <a className="bg-gray-800 text-white rounded-full px-4 py-2 hover:bg-gray-700">
             Next &raquo;
           </a>
